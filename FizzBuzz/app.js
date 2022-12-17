777d49fa2a481126fd0660de2b4d512b38d3cba3
azizbekyunusov <azizbekaliyevich@gmail.com>
2022-12-17 16:56:07 +0300

    first commit

---
+++ FizzBuzz/app.js
@@ -0,0 +1,13 @@
+function fizzBuzz(n) {
+  if(typeof n !== "number") {
+    return console.log("no number type");
+  }
+  if(n % 3 === 0 && n % 5 === 0) {
+    return console.log("FizzBuzz");
+  } else if(n % 3 === 0 && n % 5 != 0) {
+    return console.log("Fizz");
+  } else if(n % 3 != 0 && n % 5 === 0) {
+    return console.log("Buzz");
+  }
+}
+fizzBuzz("string")
---
+++ PrimeNumber/app.js
@@ -0,0 +1,15 @@
+function primeNumber(n) {
+  if (n === 1) {
+    return false;
+  } else if (n === 2) {
+    return true;
+  } else {
+    for (let x = 2; x < n; x++) {
+      if (n % x === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+console.log(primeNumber(15));
